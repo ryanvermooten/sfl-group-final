ad872fd897b7e9fb3be8ce619d00ee6313ae32ae
Ryan Vermooten <rvermooten@gmail.com>
2014-11-04 12:54:25 +0200

    made an error message for support visits while I fix it in order to ensure no data gets lost.

--- app/controllers/support_visits_controller.rb
+++ app/controllers/support_visits_controller.rb
@@ -5,8 +5,8 @@ def new
   #2nd you build a new comment                            
   @group = Group.find(params[:group_id])                  
   @gardener= Gardener.find(params[:gardener_id])
-  @support_visit= @gardener.support_visits.build       
-                                                        
+  @support_visit= @gardener.support_visits.build
+
   respond_to do |format|                                  
     format.html #new.html.erb                             
     format.xml {render :xml => @support_visit}                   
@@ -24,17 +24,16 @@ end
  
 # POST /groups/:group_id/group_gardeners
 # POST /groups/:group_id/group_gardeners.xml
+
  def create
     #1st you retrieve the group thanks to params[:group_id]
   @group = Group.find(params[:group_id])
     #2nd you create the trainer wih arguments in params [:gardener]
   @gardener= @group.gardeners.find(params[:gardener_id])
-  @support_visit = SupportVisit.new support_visit_params
   @support_visit.save
  # @gardener.support_visits.build support_visit_params
   #@support_visit= @gardener.support_visits.build support_visit_params
-
-      if @support_visit.save
+      if @support_visit.save 
         #1st argument of redirect_to is an array, in order to build the correct route to the nested resource gardener
       redirect_to group_gardener_url(@group, @gardener)
      # format.xml {render :xml => @gardener, :status => :created, :location => [@gardener.group,@gardener] }
@@ -47,10 +46,10 @@ end
   private
  
   def support_visit_params
-    params.require(:support_visit).permit(:gardener_id, :avatar, :notes, :group)
+    params.permit(:support_visit).permit(:gardener_id, :notes)
   end
  
   def gardener_params
-   params.require(:gardener).permit(:first_name, :last_name, :contact_number, :address, :group_id, :garden_at_home, :document, :id_number, :avatar)
+   params.permit(:gardener).permit(:first_name, :last_name, :contact_number, :address, :group_id, :garden_at_home, :document, :id_number, :avatar)
   end
 end
