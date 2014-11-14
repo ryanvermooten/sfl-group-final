dff490b1c2ae82e565bea819a1cdc93801ae11ca
Ryan Vermooten <rvermooten@gmail.com>
2014-11-14 17:35:08 +0200

    deleted mailer

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "notifications@sflforms.com"
-
-  def error_email(user)
-  	@user = user
-  	@url = '??'
-    mail(to: rvermooten@gmail.com, subject: 'Error message')
-  end
-end
