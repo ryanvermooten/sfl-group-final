dc088d834d7cee9eaa6213f50cc59a557640dd9a
Ryan Vermooten <rvermooten@gmail.com>
2015-02-09 12:58:04 +0200

    destroying my migrations to get this thing to pass

--- db/migrate/20150115112024_create_gardener_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateGardenerQuestions < ActiveRecord::Migration
-  def change
-    create_table :gardener_questions do |t|
-    	t.integer :question_id
-    	t.integer :gardener_id
-    end
-      add_index :gardener_questions, [:question_id, :gardener_id], unique: true,  :name => 'g_and_q_id_ix'
-  end
-end
