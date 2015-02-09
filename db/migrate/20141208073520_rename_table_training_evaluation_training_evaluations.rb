8190e986c5ca3a39c3a23b2956195c83a4abcee7
Ryan Vermooten <rvermooten@gmail.com>
2015-02-09 12:49:00 +0200

    fixing migrations issue

--- db/migrate/20141208073520_rename_table_training_evaluation_training_evaluations.rb
+++
@@ -1,5 +0,0 @@
-class RenameTableTrainingEvaluationTrainingEvaluations < ActiveRecord::Migration
-  def change
-  	rename_table :training_evaluation, :training_evaluations
-  end
-end
--- db/migrate/20141208073612_remove_user_id_from_training_evaluation.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserIdFromTrainingEvaluation < ActiveRecord::Migration
-  def change
-  	remove_column :training_evaluations, :user_id
-  	remove_column :training_evaluations, :group_id
-  end
-end
