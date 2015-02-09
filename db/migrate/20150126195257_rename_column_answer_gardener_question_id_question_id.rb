aed8b575db6922dd71841d57f462c815bdb43816
Ryan Vermooten <rvermooten@gmail.com>
2015-02-09 13:06:07 +0200

    fixing syntax error in remove_column_answer_gardener_question_id migration

--- db/migrate/20150126195257_rename_column_answer_gardener_question_id_question_id.rb
+++ db/migrate/20150126195257_rename_column_answer_gardener_question_id_question_id.rb
@@ -1,5 +1,5 @@
 class RenameColumnAnswerGardenerQuestionIdQuestionId < ActiveRecord::Migration
   def change
-  	rename_column :answers, :gardener_question_id :question_id
+  	rename_column :answers, :gardener_question_id, :question_id
   end
 end
