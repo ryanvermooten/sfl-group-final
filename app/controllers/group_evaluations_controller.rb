3bbdaa84d1150bc080bc964e3e011a56cf5094c2
Ryan Vermooten <rvermooten@gmail.com>
2015-02-17 15:59:42 +0200

    finished the forms and logic for group evaluation form

--- app/controllers/group_evaluations_controller.rb
+++ app/controllers/group_evaluations_controller.rb
@@ -1,12 +1,45 @@
 class GroupEvaluationsController <ApplicationController
 	def new
+		session[:group_evaluation_params] ||= {}
 		@group = Group.all
 		@group_evaluation = GroupEvaluation.new
 	end
+
 	def edit
 	end
+
 	def index
 	end
+
 	def create
+		session[:group_evaluation_params].deep_merge!(params[:group_evaluation]) if params[:group_evaluation]
+		@group_evaluation = GroupEvaluation.new(session[:group_evaluation_params])
+		@group_evaluation.current_step = session[:group_evaluation_step]
+
+		if params[:back_button]
+			@group_evaluation.previous_step
+		elsif @group_evaluation.last_step?
+			@group_evaluation.save
+		else
+			@group_evaluation.next_step
+		end
+
+		session[:group_evaluation_step] = @group_evaluation.current_step
+		if @group_evaluation.new_record?
+			render "new"
+		else
+			session[:group_evaluation_step] = session[:group_evaluation_params] = nil
+			flash[:notice] = "evaluation saved"
+			redirect_to :action => "new"
+		end
+	end
+
+	private
+	def group_evaluation_params
+		params.require(:group_evaluation).permit(:group_id, :date_answered, :training_good,
+    :training_information_helpful, :training_support_helpful, :training_qualitative_positive,
+    :training_qualitative_negative, :trainer_good, :trainer_understandable, :trainer_helpful,
+    :trainer_approachable, :trainer_qualitative_positive, :trainer_qualitative_negative,
+    :msc_qualitative, :final_notes, :date_answered, :training_understandable)
 	end
 end
--- app/models/group_evaluation.rb
+++ app/models/group_evaluation.rb
@@ -1,7 +1,26 @@
 class GroupEvaluation <ActiveRecord::Base
-	belongs_to :group
-	belongs_to :training_eval
-	belongs_to :trainer_eval
-	belongs_to :msc_eval
-	belongs_to :final_notes
+	attr_writer :current_step
+
+	def current_step
+		@current_step || steps.first
+	end
+	def steps
+		%w[group_and_date training trainer msc]
+	end
+
+	def next_step
+		self.current_step = steps[steps.index(current_step)+1]
+	end
+
+	def previous_step
+		self.current_step = steps[steps.index(current_step)-1]
+	end
+
+	def first_step?
+		current_step == steps.first
+	end
+
+	def last_step?
+		current_step == steps.last
+	end
 end
